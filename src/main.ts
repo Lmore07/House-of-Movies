07eb31593f3692c2d0680b956496c97578f2acaf
Luis Moreirac <lmoreirat@uteq.edu.ec>
2023-11-13 11:34:51 -0500

    cambios

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 import { inject } from '@vercel/analytics';
- 
+import { APP_INITIALIZER, isDevMode } from '@angular/core';
+
 inject();
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    providers: [
+      {
+        provide: APP_INITIALIZER,
+        useFactory: () => {
+          inject({ mode: isDevMode() ? 'development' : 'production' });
+        },
+      },
+    ],
+  })
+  .catch((err) => console.error(err));
