698825f0330853df46ab568cb731095a74db4895
Luis Moreirac <lmoreirat@uteq.edu.ec>
2024-02-10 06:07:26 -0500

    Actualizado el token de autenticación

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  TOKEN:"BkpphkeM3thAJZDlw99WV",
+  TOKEN:"r9-tNtmBb6B6xzMqQYfOR",
   API_BASE_URL: 'https://movies-proxy.vercel.app',
   API_BASE_URL_VIEW: 'https://wwv.cuevana8.com',
   API_BASE_URL_VIEW_2:"https://i2.cuevana3.nu"
