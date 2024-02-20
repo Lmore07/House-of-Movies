ebac3b07ad428bd2eea34fa52b1e3bdc8048163c
Luis Moreirac <lmoreirat@uteq.edu.ec>
2024-02-20 13:29:36 -0500

    mk

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  TOKEN:"wveutHCwJlcAIKUOhcKLp",
+  TOKEN:"G2nCQrZbbkaEJ6Cj4i9lT",
   API_BASE_URL: 'https://movies-proxy.vercel.app',
   API_BASE_URL_VIEW: 'https://wwv.cuevana8.com',
   API_BASE_URL_VIEW_2:"https://i2.cuevana3.nu"
