90cdb4a1fc1640171da31229b2e46c51c1577aa1
Lmore07 <lmoreirat@uteq.edu.ec>
2024-04-04 12:34:59 -0500

    nuevo token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
 
 export const environment = {
   production: false,
-  TOKEN:"s4hnGLwv90ousSCn0_4AA",
+  TOKEN:"TQHqRQ8Fb1PD2v8eiGm33",
   API_BASE_URL: 'https://movies-proxy.vercel.app',
   API_BASE_URL_VIEW: 'https://wwv.cuevana8.com',
   API_BASE_URL_VIEW_2:"https://i2.cuevana3.nu"
-  //api:"https://duolearn-programming.onrender.com"
 };
