b25f308883cc5ad3a1ae56db15962e6e5453f14b
Lmore07 <lmoreirat@uteq.edu.ec>
2024-04-26 09:46:37 -0500

    cambio de token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: false,
   TOKEN:"TQHqRQ8Fb1PD2v8eiGm33",
   API_BASE_URL: 'https://movies-proxy.vercel.app',
-  API_BASE_URL_VIEW: 'https://wwv.cuevana8.com',
+  API_BASE_URL_VIEW: 'https://www.cuevana8.com',
   API_BASE_URL_VIEW_2:"https://i2.cuevana3.nu"
 };
