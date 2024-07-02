3a75dca8b18ba21e15b5db75dbe404253143ed74
Lmore07 <85135347+Lmore07@users.noreply.github.com>
2024-07-02 02:37:49 +0000

    token nuevo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  TOKEN:"tiupke2sE8vfhLIm0-bVV",
+  TOKEN:"AKc4DKIrid5ccJezbHvfj",
   API_BASE_URL: 'https://movies-proxy.vercel.app',
   API_BASE_URL_VIEW: 'https://www.cuevana8.com',
   API_BASE_URL_VIEW_2:"https://i2.cuevana3.nu"
