603f23ff6cdf2924ccd9e041ab9e1ec292ad4995
Lmore07 <85135347+Lmore07@users.noreply.github.com>
2024-12-19 16:42:12 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  TOKEN:"uXzFgxUhy8sMlVDx1Gjyj",
+  TOKEN:"KHgy4-AhuCmYq-NjzM7tI",
   API_BASE_URL: 'https://movies-proxy.vercel.app',
   API_BASE_URL_VIEW: 'https://www.cuevana8.com',
   API_BASE_URL_VIEW_2:"https://i2.cuevana3.nu"
